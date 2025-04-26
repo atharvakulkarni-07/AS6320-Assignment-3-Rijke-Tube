b05bd974052cd1c4de26ae52be308e3b670d62fb
atharvakulkarni-07 <atharvakulkarni.official@gmail.com>
2025-04-26 10:29:12 +0530

    Codes for assignment 3 of Rijke Tube

---
+++ Galerkin_Modes.m
@@ -0,0 +1,124 @@
+% Simulation configuration
+steps = 1000;      % Total number of steps (N)
+duration = 100;     % Duration of experiment (T)
+delta_t = duration / steps;  % (dt)
+
+% Physical constants and flow conditions
+gamma = 1.4; % (g)
+c_sound = 399.6; % (c0)
+velocity_mean = 0.5; % (u0)
+Mach = velocity_mean / c_sound; % (M)
+% gain = 0.6;  % (K)
+gain = 0.01; 
+% gain = 1; 
+
+x_flame = 0.29; % (xf)
+% lag_time = 0.5; % (tau)
+% lag_time = 0.5;
+lag_time = 0.45 * pi;
+
+% Mode and state initialization
+modes = 3; % (J)
+eta = zeros(modes, steps); % (y1)
+eta_dot = zeros(modes, steps); % (y2)
+vel_prime = zeros(1, steps); % (u)
+pres_prime = zeros(1, steps); % (p)
+
+% Initial conditions
+eta(:,1) = 0; 
+% eta(1,1) = 0.5
+% eta(1,1) = 0.2
+eta(1,1) = 0.18;     % Excitation for mode-1
+eta_dot(:,1) = 0;
+vel_prime(1) = eta(1,1) * cos(pi * x_flame);
+pres_prime(1) = 0;
+delay_idx = round(lag_time / delta_t);
+
+% ---- Phase 1: Pre-combustion ----
+for t = 1:delay_idx
+    for m = 1:modes
+        [dy1, dy2] = preCombustion(eta(m,t), eta_dot(m,t), m);
+        [k2, l2] = preCombustion(eta(m,t) + 0.5*delta_t*dy1, eta_dot(m,t) + 0.5*delta_t*dy2, m);
+        [k3, l3] = preCombustion(eta(m,t) + 0.5*delta_t*k2, eta_dot(m,t) + 0.5*delta_t*l2, m);
+        [k4, l4] = preCombustion(eta(m,t) + delta_t*k3, eta_dot(m,t) + delta_t*l3, m);
+        eta(m,t+1) = eta(m,t) + (delta_t/6)*(dy1 + 2*k2 + 2*k3 + k4);
+        eta_dot(m,t+1) = eta_dot(m,t) + (delta_t/6)*(dy2 + 2*l2 + 2*l3 + l4);
+
+        vel_prime(t+1) = vel_prime(t+1) + eta(m,t+1) * cos(m*pi*x_flame);
+        pres_prime(t+1) = pres_prime(t+1) + eta_dot(m,t+1) * ((-gamma*Mach)/(m*pi)) * sin(m*pi*x_flame);
+    end
+end
+
+% ---- Phase 2: Post-combustion with feedback ----
+for t = delay_idx+1:steps-1
+    for m = 1:modes
+        [dy1, dy2] = postCombustion(eta(m,t), eta_dot(m,t), m, gain, vel_prime(t - delay_idx), x_flame);
+        [k2, l2] = postCombustion(eta(m,t) + 0.5*delta_t*dy1, eta_dot(m,t) + 0.5*delta_t*dy2, m, gain, vel_prime(t - delay_idx), x_flame);
+        [k3, l3] = postCombustion(eta(m,t) + 0.5*delta_t*k2, eta_dot(m,t) + 0.5*delta_t*l2, m, gain, vel_prime(t - delay_idx), x_flame);
+        [k4, l4] = postCombustion(eta(m,t) + delta_t*k3, eta_dot(m,t) + delta_t*l3, m, gain, vel_prime(t - delay_idx), x_flame);
+
+        eta(m,t+1) = eta(m,t) + (delta_t/6)*(dy1 + 2*k2 + 2*k3 + k4);
+        eta_dot(m,t+1) = eta_dot(m,t) + (delta_t/6)*(dy2 + 2*l2 + 2*l3 + l4);
+
+        vel_prime(t+1) = vel_prime(t+1) + eta(m,t+1) * cos(m*pi*x_flame);
+        pres_prime(t+1) = pres_prime(t+1) + eta_dot(m,t+1) * ((-gamma*Mach)/(m*pi)) * sin(m*pi*x_flame);
+    end
+end
+
+% ---- Energy Analysis ----
+energy = (0.5 * (pres_prime.^2) + 0.5 * (gamma * Mach * vel_prime).^2) / ((gamma * Mach)^2); % (e) 
+env_energy = envelope(energy, 200, 'peak'); % (ee)
+
+% ---- Visualization ----
+time_vec = linspace(0, duration, steps);
+
+figure(1);
+plot(time_vec, vel_prime, 'b', 'LineWidth', 2);
+title("u' Over Time"); xlabel('Time (s)'); ylabel("u'"); grid on;
+
+figure(2);
+plot(time_vec, eta(1,:), 'm', 'LineWidth', 2);
+title('\eta_1(t)'); xlabel('Time (s)'); ylabel('\eta_1'); grid on;
+
+figure(3);
+plot(time_vec, eta(2,:), 'm', 'LineWidth', 2);
+title('\eta_2(t)'); xlabel('Time (s)'); ylabel('\eta_2'); grid on;
+
+figure(4);
+plot(time_vec, eta(3,:), 'm', 'LineWidth', 2);
+title('\eta_3(t)'); xlabel('Time (s)'); ylabel('\eta_3'); grid on;
+
+figure(5);
+plot(time_vec, env_energy, 'r', 'LineWidth', 2);
+title('Amplitude of Energy Envelope'); xlabel('Time (s)');
+ylabel('Normalized Energy'); legend('Nonlinear'); grid on;
+
+figure(6);
+tiledlayout(2,2);
+nexttile; plot(time_vec, vel_prime, 'b', 'LineWidth', 2); title("u'(t)");
+nexttile; plot(time_vec, eta(1,:), 'g', 'LineWidth', 2); title('\eta_1');
+nexttile; plot(time_vec, eta(2,:), 'g', 'LineWidth', 2); title('\eta_2');
+nexttile; plot(time_vec, eta(3,:), 'g', 'LineWidth', 2); title('\eta_3');
+ylim([-0.4 0.4]);
+
+% ---- Governing Equations ----
+function [d1, d2] = preCombustion(a, b, idx)
+    k = idx*pi;
+    omega = k;
+    base_freq = pi;
+    c1 = 0.1; c2 = 0.06;
+    damping = (1/(2*pi))*(c1*(omega/base_freq) + c2*sqrt(base_freq/omega));
+    d1 = b;
+    d2 = -k^2 * a - 2*damping*omega*b;
+end
+
+function [d1, d2] = postCombustion(a, b, idx, gain, delayed_u, pos_x)
+    k = idx*pi;
+    omega = k;
+    base_freq = pi;
+    c1 = 0.1; c2 = 0.06;
+    damping = (1/(2*pi))*(c1*(omega/base_freq) + c2*sqrt(base_freq/omega));
+    source_term = (2*k*gain)*(sqrt(abs(1/3 + delayed_u)) - sqrt(1/3)) * sin(idx*pi*pos_x);
+    d1 = b;
+    d2 = -k^2 * a - 2*damping*omega*b - source_term;
+end
---
+++ bifurcation_final_script.m
@@ -0,0 +1,181 @@
+% Simulation parameters
+numSteps    = 1000;     % number of timesteps
+% total duration of experiment
+totalTime   = 40;         % total time of experiment
+timeStep    = totalTime / numSteps; % dt
+flamePos    = 0.3;       % m (original xf)
+
+% Physical constants
+gammaVal       = 1.4;    % g
+speedOfSound   = 399.6;  % c0
+baseVelocity   = 0.5;   % u0
+MachNum        = baseVelocity / speedOfSound; % M
+numModes       = 3;   % number of Galerkin modes (J)
+
+% Parameter sweeps
+gainList       = 0.2:0.01:1.4; % non-dimensional heater power (ks)
+numGains       = length(gainList); % n1
+delayList      = 0.2:0.01:0.8; % time-lag (tau)
+numDelays      = length(delayList);% n3
+
+% Preallocate output arrays
+rmsForward     = zeros(numDelays, numGains);   % u_rms1
+dynStateA      = zeros(numDelays, numModes, numSteps);  % y1
+dynStateB      = zeros(numDelays, numModes, numSteps);  % y2
+
+% Forward integration loop
+for delayIdx = 1:numDelays
+    % Initialize state for this delay
+    dynStateA(delayIdx,:,:) = 0;  % y1
+    dynStateB(delayIdx,:,:) = 0;  % y2
+    dynStateA(delayIdx,1,1) = 0.18;  % initial mode-1 excitation
+
+    velHistF = zeros(numGains, numSteps); % u1 history
+
+    for gainIdx = 1:numGains
+        % Compute initial velocity perturbation
+        velHistF(gainIdx,1) = 0;
+        for modeIdx = 1:numModes
+            velHistF(gainIdx,1) = velHistF(gainIdx,1) + dynStateA(delayIdx,modeIdx,1) * ...
+                                   cos(modeIdx*pi*flamePos);
+        end
+
+        lagSteps = round(delayList(delayIdx) / timeStep); % n2
+
+        % Phase 1: t < tau (no heat feedback)
+        for t = 1:lagSteps
+            for modeIdx = 1:numModes
+                [k1a, l1a] = dynNoHeat(dynStateA(delayIdx,modeIdx,t),   dynStateB(delayIdx,modeIdx,t),   modeIdx);
+                [k2a, l2a] = dynNoHeat(dynStateA(delayIdx,modeIdx,t)+0.5*timeStep*k1a, dynStateB(delayIdx,modeIdx,t)+0.5*timeStep*l1a, modeIdx);
+                [k3a, l3a] = dynNoHeat(dynStateA(delayIdx,modeIdx,t)+0.5*timeStep*k2a, dynStateB(delayIdx,modeIdx,t)+0.5*timeStep*l2a, modeIdx);
+                [k4a, l4a] = dynNoHeat(dynStateA(delayIdx,modeIdx,t) + timeStep*k3a,       dynStateB(delayIdx,modeIdx,t)+timeStep*l3a, modeIdx);
+
+                dynStateA(delayIdx,modeIdx,t+1) = dynStateA(delayIdx,modeIdx,t) + (timeStep/6)*(k1a + 2*k2a + 2*k3a + k4a);
+                dynStateB(delayIdx,modeIdx,t+1) = dynStateB(delayIdx,modeIdx,t) + (timeStep/6)*(l1a + 2*l2a + 2*l3a + l4a);
+
+                velHistF(gainIdx,t+1) = velHistF(gainIdx,t+1) + dynStateA(delayIdx,modeIdx,t+1) * cos(modeIdx*pi*flamePos);
+            end
+        end
+
+        % Phase 2: t > tau (with heat feedback)
+        for t = lagSteps+1:numSteps-1
+            for modeIdx = 1:numModes
+                [k1b, l1b] = dynWithHeat(dynStateA(delayIdx,modeIdx,t),   dynStateB(delayIdx,modeIdx,t),   modeIdx, gainList(gainIdx), velHistF(gainIdx,t-lagSteps), flamePos);
+                [k2b, l2b] = dynWithHeat(dynStateA(delayIdx,modeIdx,t)+0.5*timeStep*k1b, dynStateB(delayIdx,modeIdx,t)+0.5*timeStep*l1b, modeIdx, gainList(gainIdx), velHistF(gainIdx,t-lagSteps), flamePos);
+                [k3b, l3b] = dynWithHeat(dynStateA(delayIdx,modeIdx,t)+0.5*timeStep*k2b, dynStateB(delayIdx,modeIdx,t)+0.5*timeStep*l2b, modeIdx, gainList(gainIdx), velHistF(gainIdx,t-lagSteps), flamePos);
+                [k4b, l4b] = dynWithHeat(dynStateA(delayIdx,modeIdx,t)+timeStep*k3b,       dynStateB(delayIdx,modeIdx,t)+timeStep*l3b,       modeIdx, gainList(gainIdx), velHistF(gainIdx,t-lagSteps), flamePos);
+
+                dynStateA(delayIdx,modeIdx,t+1) = dynStateA(delayIdx,modeIdx,t) + (timeStep/6)*(k1b + 2*k2b + 2*k3b + k4b);
+                dynStateB(delayIdx,modeIdx,t+1) = dynStateB(delayIdx,modeIdx,t) + (timeStep/6)*(l1b + 2*l2b + 2*l3b + l4b);
+
+                velHistF(gainIdx,t+1) = velHistF(gainIdx,t+1) + dynStateA(delayIdx,modeIdx,t+1) * cos(modeIdx*pi*flamePos);
+            end
+        end
+
+        % Finalize RMS for forward pass
+        dynStateA(delayIdx,:,1) = dynStateA(delayIdx,:,numSteps);
+        dynStateB(delayIdx,:,1) = dynStateB(delayIdx,:,numSteps);
+        rmsForward(delayIdx,gainIdx) = rms(velHistF(gainIdx,:));
+    end
+end
+
+% Backward integration (mirror of forward)
+rmsBackward = zeros(numDelays, numGains);
+for delayIdx = 1:numDelays
+    velHistB = zeros(numGains, numSteps);
+    for gainIdx = 1:numGains
+        revIdx = numGains - gainIdx + 1; % reversed index
+        % Initialize reversed velocity
+        velHistB(revIdx,1) = 0;
+        for modeIdx = 1:numModes
+            velHistB(revIdx,1) = velHistB(revIdx,1) + dynStateA(delayIdx,modeIdx,1) * cos(modeIdx*pi*flamePos);
+        end
+        lagSteps = round(delayList(delayIdx) / timeStep);
+
+        % t < tau (no heat)
+        for t = 1:lagSteps
+            for modeIdx = 1:numModes
+                [k1c, l1c] = dynNoHeat(dynStateA(delayIdx,modeIdx,t),   dynStateB(delayIdx,modeIdx,t),   modeIdx);
+                [k2c, l2c] = dynNoHeat(dynStateA(delayIdx,modeIdx,t) + 0.5*timeStep*k1c, dynStateB(delayIdx,modeIdx,t) + 0.5*timeStep*l1c, modeIdx);
+                [k3c, l3c] = dynNoHeat(dynStateA(delayIdx,modeIdx,t) + 0.5*timeStep*k2c, dynStateB(delayIdx,modeIdx,t) + 0.5*timeStep*l2c, modeIdx);% corrected line
+                [k4c, l4c] = dynNoHeat(dynStateA(delayIdx,modeIdx,t) + timeStep*k3c,       dynStateB(delayIdx,modeIdx,t)+ timeStep*l3c, modeIdx);
+
+                dynStateA(delayIdx,modeIdx,t+1) = dynStateA(delayIdx,modeIdx,t) + (timeStep/6)*(k1c + 2*k2c + 2*k3c + k4c);
+                dynStateB(delayIdx,modeIdx,t+1) = dynStateB(delayIdx,modeIdx,t) + (timeStep/6)*(l1c + 2*l2c + 2*l3c + l4c);
+
+                velHistB(revIdx,t+1) = velHistB(revIdx,t+1) + dynStateA(delayIdx,modeIdx,t+1) * cos(modeIdx*pi*flamePos);
+            end
+        end
+
+        % t > tau (with heat)
+        for t = lagSteps+1:numSteps-1
+            for modeIdx = 1:numModes
+                [k1d, l1d] = dynWithHeat(dynStateA(delayIdx,modeIdx,t),dynStateB(delayIdx,modeIdx,t),   modeIdx, gainList(revIdx), velHistB(revIdx,t-lagSteps), flamePos);
+                [k2d, l2d] = dynWithHeat(dynStateA(delayIdx,modeIdx,t)+0.5*timeStep*k1d, dynStateB(delayIdx,modeIdx,t)+0.5*timeStep*l1d, modeIdx, gainList(revIdx),velHistB(revIdx,t-lagSteps), flamePos);
+                [k3d, l3d] = dynWithHeat(dynStateA(delayIdx,modeIdx,t)+0.5*timeStep*k2d, dynStateB(delayIdx,modeIdx,t)+0.5*timeStep*l2d, modeIdx,gainList(revIdx), velHistB(revIdx,t-lagSteps), flamePos);% corrected line
+                [k4d, l4d] = dynWithHeat(dynStateA(delayIdx,modeIdx,t)+timeStep*k3d,       dynStateB(delayIdx,modeIdx,t)+timeStep*l3d,       modeIdx, gainList(revIdx),velHistB(revIdx,t-lagSteps), flamePos);
+
+                dynStateA(delayIdx,modeIdx,t+1) = dynStateA(delayIdx,modeIdx,t) + (timeStep/6)*(k1d + 2*k2d + 2*k3d + k4d);
+                dynStateB(delayIdx,modeIdx,t+1) = dynStateB(delayIdx,modeIdx,t) + (timeStep/6)*(l1d + 2*l2d + 2*l3d + l4d);
+
+                velHistB(revIdx,t+1) = velHistB(revIdx,t+1) + dynStateA(delayIdx,modeIdx,t+1) * cos(modeIdx*pi*flamePos);
+            end
+        end
+
+        % Finalize RMS for backward
+dynStateA(delayIdx,:,1) = dynStateA(delayIdx,:,numSteps);
+dynStateB(delayIdx,:,1) = dynStateB(delayIdx,:,numSteps);
+rmsBackward(delayIdx,revIdx) = rms(velHistB(revIdx,:));
+    end
+end
+
+% Plot results (standard style, using scatter plot)
+timeVec = linspace(0, totalTime, numSteps);
+for delayIdx = 1:numDelays
+    if abs(delayList(delayIdx) - 0.2) < 1e-6
+        figure(1);
+        scatter(gainList, rmsForward(delayIdx,:), 20, 'b', 'filled'); hold on;
+        scatter(gainList, rmsBackward(delayIdx,:), 20, 'r', 'filled');
+        xlabel('K');
+        ylabel('|u_1|');
+        legend('Forward', 'Backward');
+        title('Bifurcation Plot');
+        annotation('textarrow', [0.7 0.6], [0.3 0.15], 'String', 'Hopf Bifurcation');
+        annotation('textarrow', [0.28 0.3], [0.8 0.55], 'String', 'Fold Bifurcation');
+        grid on;
+    end
+end
+
+
+figure(2);
+mesh(rmsForward); hold on;
+mesh(rmsBackward);
+waterfall(gainList, delayList, rmsForward); hold on;
+waterfall(gainList, delayList, rmsBackward);
+xlabel('K');
+ylabel('tau');
+zlabel('|u_1|');
+legend('Forward','Backward');
+title('3D Bifurcation Plot');
+annotation('textarrow', [0.6 0.45], [0.3 0.15], 'String', 'Hopf Bifurcation');
+annotation('textarrow', [0.2 0.25], [0.6 0.5], 'String', 'Fold Bifurcation');
+colorbar;
+
+
+% Function definitions
+function [fA,fB] = dynNoHeat(aState, bState, modeIdx)
+    kVal = modeIdx*pi; omega = kVal; base = pi;
+    c1 = 0.1; c2 = 0.06;
+    damp = (1/(2*pi)) * (c1*(omega/base) + c2*sqrt(base/omega));
+    fA = bState;
+    fB = -2*damp*omega*bState - kVal^2 * aState;
+end
+
+function [fA,fB] = dynWithHeat(aState, bState, modeIdx, Kgain, delayedVel, pos)
+    kVal = modeIdx*pi; omega = kVal; base = pi;
+    c1 = 0.1; c2 = 0.06;
+    damp = (1/(2*pi)) * (c1*(omega/base) + c2*sqrt(base/omega));
+    source = modeIdx*pi*Kgain * (abs(sqrt(1/3 + delayedVel)) - sqrt(1/3)) * sin(modeIdx*pi*pos);
+    fA = bState;
+    fB = -2*damp*omega*bState - kVal^2 * aState - source;
+end
